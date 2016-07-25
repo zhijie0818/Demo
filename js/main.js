7cb06c3c2d53da1fafc95a563bf7fc6ca3a42832
zj <zhijie@zwoil.com>
2016-07-25 09:55:02 +0800

    tt

---
+++ js/main.js
@@ -0,0 +1,23 @@
+new function (){
+   var _self = this;
+   _self.width = 640;//设置默认最大宽度
+   _self.fontSize = 100;//默认字体大小
+   _self.widthProportion = function(){var p = (document.body&&document.body.clientWidthdocument.getElementsByTagName("html")[0].offsetWidth)/_self.width;return p>1?1:p<0.5?0.5:p;};
+   _self.changePage = function(){
+       document.getElementsByTagName("html")[0].setAttribute("style","font-size:"+_self.widthProportion()*_self.fontSize+"px !important");
+   }
+   _self.changePage();
+   window.addEventListener("resize",function(){_self.changePage();},false);
+};
+// 	(function (doc, win) {
+// 	var docEl = doc.documentElement,
+// 	clientWidth = null,
+// 	resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+// 	recalc = function () {
+// 		clientWidth = docEl.clientWidth;
+// 		docEl.style.fontSize = 100 * (clientWidth / 320) + 'px';
+// 	};
+// 	recalc();
+// 	if (!doc.addEventListener) return; 
+// 	win.addEventListener(resizeEvt, recalc, false);
+// })(document, window);
